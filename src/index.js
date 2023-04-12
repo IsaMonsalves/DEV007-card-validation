04a9ebb82714fd0cc425d1a85722e98bd25b579e
IsaMonsalves <ester.isabella.m@gmail.com>
2023-04-12 09:44:41 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import validator from './validator.js';
 
 console.log(validator);
 
-var refresh= document.getElementById("refresh"); 
-refresh.addEventListener("click"{location.reload();})
+function refresh ()
+{
+    location.reload(); 
+   
+}
+
