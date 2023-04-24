d5502b4c1bc610c7308b6a6b465a615c07bab421
IsaMonsalves <ester.isabella.m@gmail.com>
2023-04-23 22:39:53 -0400

    quinto commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-function refresh()
-{
- location.reload();  
+import validator from "./validator.js";
+
+function refresh() {
+  location.reload();
 }
 
+function maskify() {
+  const number = document.getElementById("creditCardNumber").value;
+  const acumulador = validator.maskify(number);
+  console.log(number);
+  document.getElementById("creditCardNumber").value = acumulador;
+}
 
+function isValid() {
+  const creditCardNumber = document.getElementById("creditCardNumber").value;
+  if (creditCardNumber.length < 12 || creditCardNumber.length > 18) {
+    alert("Su tarjeta debe tener entre 12 y 18 números");
+    return;
+  }
 
-import validator from './validator.js';
+  const validate = validator.isValid(creditCardNumber);
+  if (validate === true) {
+    alert("El número de tarjeta Si corresponde a una tarjeta de crédito");
+  } else {
+    alert("El número de tarjeta No coresponde a una tarjeta de crédito ");
+  }
+}
 
-
-
-
-
+//Creo variables para poder usar las funciones en el html
+window.isValid = isValid;
+window.refresh = refresh;
+window.maskify = maskify;
--- src/validator.js
+++ src/validator.js
@@ -1,62 +1,52 @@
-
- function isValid(){
-let creditCardNumber= document.getElementById("creditCardNumber").value
-if(creditCardNumber.length < 12 || creditCardNumber.length >18){
-  alert("Su tarjeta debe tener entre 12 y 18 números");
-  return;
-}
-  let reverse = creditCardNumber.toString().split("").reverse();
-  for(let i = 0; i < reverse.length; i++){
-    if((i+1) % 2 == 0)
-    {
-      reverse[i] = reverse[i] * 2;
+const validator = {
+  isValid: (creditCardNumber) => {
+    //const creditCardNumber = document.getElementById("creditCardNumber").value;
+    /* if (creditCardNumber.length < 12 || creditCardNumber.length > 18) {
+      alert("Su tarjeta debe tener entre 12 y 18 números");
+      return;
     }
-    if(reverse[i] >= 10){
-      reverse[i] = reverse[i] - 9;
-    }
-  }
-  let total = 0;
-  for(let i = 0; i < reverse.length; i++)
-  {
-    total = total + parseInt(reverse[i]);
-  }
-  let validate = false;
-  if(total % 10 == 0)
-  {
-    validate = true;
-  }
-  console.log(total);
-  console.log(validate);
-  if(validate==true){
-    alert("El número de tarjeta Si corresponde a una tarjeta de crédito");
-  } else {
-    alert("El número de tarjeta No coresponde a una tarjeta de crédito ");
-  }
-  return validate;
-}
-
-function maskify(){
-  let number = document.getElementById("creditCardNumber").value;
-  let acumulador= esconder(number);
-  console.log(number);
-  document.getElementById("creditCardNumber").value = acumulador;
-}
-  function esconder (creditCardNumber) {
-    creditCardNumber= creditCardNumber.toString();
-    let acumulador="";
-      for(let i = 0; i<creditCardNumber.length; i++) {
-        if (i<=12){
-          acumulador= acumulador + "#"; 
-        } else {
-          acumulador= acumulador + creditCardNumber[i];
-        }
+    */
+    const reverse = creditCardNumber.toString().split("").reverse();
+    for (let i = 0; i < reverse.length; i++) {
+      if ((i + 1) % 2 === 0) {
+        reverse[i] = reverse[i] * 2;
       }
-      console.log(acumulador);
-      console.log(creditCardNumber);
-return acumulador;
-  }
- 
-  export default validator;
+      if (reverse[i] >= 10) {
+        reverse[i] = reverse[i] - 9;
+      }
+    }
+    let total = 0;
+    for (let i = 0; i < reverse.length; i++) {
+      total = total + parseInt(reverse[i]);
+    }
+    let validate = false;
+    if (total % 10 === 0) {
+      validate = true;
+    }
+    console.log(total);
+    console.log(validate);
+    /*  if (validate === true) {
+      alert("El número de tarjeta Si corresponde a una tarjeta de crédito");
+    } else {
+      alert("El número de tarjeta No coresponde a una tarjeta de crédito ");
+    }*/
+    return validate;
+  },
+  maskify: (creditCardNumber) => {
+    if (creditCardNumber.length < 5) {
+      return creditCardNumber;
+    }
+    creditCardNumber = creditCardNumber.toString();
+    const arreglo = creditCardNumber.split("");
 
+    for (let i = 0; i < arreglo.length - 4; i++) {
+      arreglo[i] = "#";
+    }
 
+    creditCardNumber = arreglo.join("");
 
+    return creditCardNumber;
+  },
+};
+
+export default validator;
